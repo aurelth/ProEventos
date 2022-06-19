5ccd5eb36dfe503f09e2e3d72e568fd31b782a6c
aurelth <aurelth@icloud.com>
2022-06-19 13:35:39 -0300

    Improving the Screen by adding a Filter and other functionalities.

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -7,7 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.scss'],
 })
 export class EventosComponent implements OnInit {
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+  public widthImg: number = 100;
+  public marginImg: number = 2;
+  public showImage: boolean = true;
+  private _filtroLista: string = '';
+
+  public get filtroLista() {
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista
+      ? this.filtrarEventos(this.filtroLista)
+      : this.eventos;
+  }
+
+  public filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: { tema: any; local: any }) =>
+        evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+        evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
 
   constructor(private http: HttpClient) {}
 
@@ -17,8 +42,15 @@ export class EventosComponent implements OnInit {
 
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/evento').subscribe(
-      (response) => (this.eventos = response),
+      (response) => {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos;
+      },
       (error) => console.log(error)
     );
+  }
+
+  public showImageOnScreen(): void {
+    this.showImage = !this.showImage;
   }
 }
